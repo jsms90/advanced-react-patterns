9fda390bbae58ca8011bb9de53b5ba82bd84d229
Jennifer Spencer <jennifer.spencer@yld.io>
2018-12-06 12:32:30 +0000

    1st exercise - demo

--- src/components/functional-programming/composition/example/index.js
+++ src/components/functional-programming/composition/example/index.js
@@ -1,5 +1,6 @@
 export const compose = (...functions) => initialValue =>
-  functions.reduceRight((currentValue, fn) => fn(currentValue), initialValue);
+functions.reduceRight((currentValue, fn) => fn(currentValue), initialValue);
+// student asks whether this is the wrong way round
 
 const toUpperCase = text => text.toUpperCase();
 
@@ -7,10 +8,50 @@ const removeSpaces = text => text.replace(/\s/g, "");
 
 const removeNumbers = text => text.replace(/[0-9]/g, "");
 
-// export const transformText = compose(
-//   toUpperCase,
-//   removeNumbers,
-//   removeSpaces
-// );
+export const transformText = compose(
+  toUpperCase,
+  removeNumbers,
+  removeSpaces
+);
 
-export const transformText = text => text;
+// This line was originally not commented out
+// export const transformText = text => text;
+
+
+
+/*
+
+Showing students how to get here:
+
+const exampleText = "1 2 3 ReactJS Academy is a m a z i n g" 
+
+// Start with:
+
+const myTransformer = toUpperCase(exampleText)
+const myTransformer2 = removeSpaces(myTransformer)
+const myTransformer3 = removeNumberrs(myTransformer2)
+
+
+// Refine:
+
+const answer = removeNumberrs(removeSpaces(toUpperCase(exampleText)))
+
+
+// Create compose function that takes in a set of functions and calls them one after another
+
+const compose = (...functions) => initialValue => {
+  functions.reduce((accumulator, currentValue) => {
+    currentValue(accumulator)       // He wrote something like this. I didn't copy in time
+  }, initialValue)
+}
+
+const compose = (...functions) => initialValue =>
+  functions.reduceRight((currentValue, fn) => fn(currentValue), initialValue);
+
+const transformText = compose(
+    toUpperCase,
+    removeNumbers,
+    removeSpaces
+  );
+  
+*/
